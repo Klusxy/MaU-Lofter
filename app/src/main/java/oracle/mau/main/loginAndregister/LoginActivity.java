8be9606bc98acfdd344633bf7965c6b35c022d01
Your Name <shadowgjy@126.com>
2017-02-27 18:04:12 +0800

    测试

---
+++ app/src/main/java/oracle/mau/main/loginAndregister/LoginActivity.java
@@ -0,0 +1,19 @@
+package oracle.mau.main.loginAndregister;
+
+import oracle.mau.base.BaseActivity;
+
+/**
+ * Created by shadow on 2017/2/27.
+ */
+
+public class LoginActivity  extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
