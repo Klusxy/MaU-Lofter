51ae9ec1904df5d33aa68a4bcdbb2b7642c1d31c
tianshuai <564241248@qq.com>
2017-03-14 11:50:19 +0800

    搜索功能全部完成

--- app/src/main/java/oracle/mau/main/label/adapter/SearchSpecialAdapter.java
+++ app/src/main/java/oracle/mau/main/label/adapter/SearchSpecialAdapter.java
@@ -1,13 +1,18 @@
 package oracle.mau.main.label.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
+import oracle.mau.R;
 import oracle.mau.entity.SpecialEntity;
+import oracle.mau.utils.ImageUtils;
 
 /**
  * Created by 田帅 on 2017/3/13.
@@ -24,21 +29,37 @@ public class SearchSpecialAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return list.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder vh;
+        if (convertView == null) {
+            vh = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.lv_item_search_special,null);
+//            vh.iv_lv_item_search_special_img = (ImageView) convertView.findViewById(R.id.iv_lv_item_search_special_img);
+            vh.tv_lv_item_search_special_name = (TextView) convertView.findViewById(R.id.tv_lv_item_search_special_name);
+            convertView.setTag(vh);
+        }else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+        vh.tv_lv_item_search_special_name.setText(list.get(position).getSpecialTitle());
+        return convertView;
+    }
+
+    private class ViewHolder{
+//        private ImageView iv_lv_item_search_special_img;
+        private TextView tv_lv_item_search_special_name;
     }
 }
