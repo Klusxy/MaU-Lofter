35ec30b22b8d17346454fe98d0b8d25fc457b3fc
tianshuai <564241248@qq.com>
2017-03-15 17:04:09 +0800

    解决部分手机label模块 vp跳转不流畅问题

--- app/src/main/java/oracle/mau/main/label/view/TouchViewPager.java
+++ app/src/main/java/oracle/mau/main/label/view/TouchViewPager.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import java.io.Serializable;
@@ -45,17 +46,33 @@ public class TouchViewPager extends ViewPager {
         this.tagList = tagList;
     }
 
+    float downX = 0;
+    float downY = 0;
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 vp_label_tag_flag = 0;
-
+                downX = ev.getX();
+                downY = ev.getY();
+                Log.d("dasdas" , "按下时  x坐标为  " +ev.getX());
+                Log.d("dasdas" , "按下时  y坐标为  " +ev.getY());
                 break;
             case MotionEvent.ACTION_MOVE:
                 vp_label_tag_flag = 1;
+                Log.d("dasdas" , "移动  x坐标为  " +ev.getX());
+                Log.d("dasdas" , "移动  y坐标为  " +ev.getY());
+                /**
+                 * 部分手机会出现该现象：  x y 没变  但是执行了move方法，导致跳转不过去，所以在这又加一层判断
+                 */
+                if (ev.getX() == downX) {
+                    vp_label_tag_flag = 0;
+                }
                 break;
             case MotionEvent.ACTION_UP:
+                Log.d("dasdas" , "起  x坐标为  " +ev.getX());
+                Log.d("dasdas" , "起  y坐标为  " +ev.getY());
                 if (vp_label_tag_flag == 0) {
                     if (tagList!=null){
                         int item = this.getCurrentItem()%tagList.size();
